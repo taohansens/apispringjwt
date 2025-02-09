f96b45ced8c576468a699bc4af01f6050c74b0d7
Tao Hansen <53713149+taohansens@users.noreply.github.com>
2025-02-09 11:49:53 -0300

    [FEAT] Roles Entity

---
+++ src/main/java/br/com/hansensoft/apibackend/repository/jpa/RoleRepository.java
@@ -0,0 +1,12 @@
+package br.com.hansensoft.apibackend.repository.jpa;
+
+import br.com.hansensoft.apibackend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,6 +3,23 @@ create table tb_user
     id       bigserial primary key,
     email    varchar(255),
     password varchar(255),
-    username varchar(255),
-    role     varchar(255)
+    username varchar(255)
 );
+
+
+create table tb_roles
+(
+    id       bigserial primary key,
+    name     varchar(255) not null
+);
+
+create table user_roles
+(
+    user_id  bigint not null,
+    role_id  bigint not null,
+    primary key (user_id, role_id),
+    foreign key (user_id) references tb_user(id),
+    foreign key (role_id) references tb_roles(id)
+);
+
+insert into tb_roles (name) values ('USER'), ('ADMIN'), ('SUPERADMIN');
